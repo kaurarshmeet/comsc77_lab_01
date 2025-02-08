8a921b5fb03e8151ddeacf262be287ff7ca84870
Arshmeet Kaur <xarshmeetx@gmail.com>
2025-02-08 11:24:18 -0800

    Set up project + distributed the workload.

---
+++ Converter.java
@@ -0,0 +1,48 @@
+public class Converter {
+
+    /**
+     * Main method asks user for input in binary or decimal.
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Use Scanner
+        //TODO: ask user to enter a number in binary or decimal.
+        //TODO: ask user what type of number it is
+        //TODO: check to make sure the number is legal (a decimal number contains only 0-9). If the number is not legal, it should error out!
+        //TODO: If the number is legal, convert the number to the other 3 types (Hint: There are a total of 4 types: binary, octal, hexadecimal, decimal)
+        // => will call 3 other methods
+
+        // TODO: Use this to test your methods.
+    }
+
+
+    //TODO: Arshmeet
+    public String decimalToBinary(int decimal){
+        return "";
+    }
+
+    //TODO: bheraz2
+    public String decimalToOctal(int decimal) {
+        return "";
+    }
+
+    //TODO: kvrm7
+    public String decimalToHex(int decimal) {
+        return "";
+    }
+
+    //TODO: Arshmeet
+    public int binaryToDecimal(String binary){
+        return 0;
+    }
+
+    //TODO: bheraz2
+    public String binaryToOctal(String binary){
+        return "";
+    }
+
+    //TODO: kvrm7
+    public String binaryToHex(String binary){
+        return "";
+    }
+}
